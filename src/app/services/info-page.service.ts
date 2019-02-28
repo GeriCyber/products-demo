04a31d6f582b019e4a68c15f3374976570b17ff5
Geraldine Herrera Garmendia <gericyber@gmail.com>
2019-02-28 19:15:09 -0400

    Cargando info de (about)desde firebase

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPage } from '../interfaces/info-page.interface';
+import { InfoTeam } from '../interfaces/info-team.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,25 @@ import { InfoPage } from '../interfaces/info-page.interface';
 export class InfoPageService {
 
   info: InfoPage;
+  team: InfoTeam[] = [];
   cargada = false;
 
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     this.http.get('./assets/data/data-page.json').subscribe((data: InfoPage) => {
-        this.cargada = true;
-        this.info = data;
-        // console.log(this.info);
-      });
+      this.cargada = true;
+      this.info = data;
+    });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://portfolio-demo-d7899.firebaseio.com/equipo.json').subscribe((data: InfoTeam[]) => {
+      this.team = data;
+      console.log(this.team);
+    });
   }
 }
